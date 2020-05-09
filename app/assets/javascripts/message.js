5f715cfcc68c320cc1e1dff0c04fe8425ec75771
pochi-gh <pochigm@gmail.com>
2020-05-09 17:06:16 +0900

    add ajax message

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,56 @@
+$(function(){
+  function buildHTML(message){
+    // 「もしメッセージに画像が含まれていたら」という条件式
+    if (message.image) {
+      var html = `<div class="message__user">
+                  ${message.user_name}
+                    <div class="message__user--time">
+                    ${message.created_at}
+                    </div>
+                  </div>
+                  <div class="message__text">
+                    <p class="lower-message__content">
+                      ${message.content}
+                    </p>
+                    <img class="lower-message__image" src = ${message.image} ">
+                  </div>`
+      return html;
+    } else {
+      var html =  `<div class="message__user">
+                    ${message.user_name}
+                    <div class="message__user--time">
+                      ${message.created_at}
+                    </div>
+                  </div>
+                  <div class="message__text">
+                    <p class="lower-message__content">
+                      ${message.content}
+                    </p>
+                  </div>`
+      return html;
+    }
+  }
+  $('#new_message').on('submit',function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.chat-main__message').append(html);
+      $('.chat-main__message').animate({ scrollTop: $('.chat-main__message')[0].scrollHeight});
+      $('.form__submit').prop('disabled', false);
+      $('form')[0].reset();
+    })
+    .fail(function(){
+      alert("メッセージ送信に失敗しました");
+    })
+  });
+});
