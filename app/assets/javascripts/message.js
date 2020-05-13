3d8e2d5006cfd069e3b404344f833c8f9012fc51
pochi-gh <pochigm@gmail.com>
2020-05-13 12:29:06 +0900

    add  auto-update message

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,35 +1,65 @@
 $(function(){
+
   function buildHTML(message){
     // 「もしメッセージに画像が含まれていたら」という条件式
     if (message.image) {
-      var html = `<div class="message__user">
-                  ${message.user_name}
-                    <div class="message__user--time">
-                    ${message.created_at}
+      var html = `<div class="message" data-message-id=${message.id}>
+                    <div class="message__user">
+                      ${message.user_name}
+                      <div class="message__user--time">
+                        ${message.created_at}
+                      </div>
                     </div>
-                  </div>
-                  <div class="message__text">
-                    <p class="lower-message__content">
-                      ${message.content}
-                    </p>
-                    <img class="lower-message__image" src = ${message.image} ">
+                    <div class="message__text">
+                      <p class="lower-message__content">
+                        ${message.content}
+                      </p>
+                      <img class="lower-message__image" src = ${message.image} ">
+                    </div>
                   </div>`
       return html;
     } else {
-      var html =  `<div class="message__user">
-                    ${message.user_name}
-                    <div class="message__user--time">
-                      ${message.created_at}
+      var html =  `<div class="message" data-message-id=${message.id}>
+                    <div class="message__user">
+                      ${message.user_name}
+                      <div class="message__user--time">
+                        ${message.created_at}
+                      </div>
                     </div>
-                  </div>
-                  <div class="message__text">
-                    <p class="lower-message__content">
-                      ${message.content}
-                    </p>
-                  </div>`
+                    <div class="message__text">
+                      <p class="lower-message__content">
+                        ${message.content}
+                      </p>
+                    </div>
+                  </div>  `
       return html;
-    }
-  }
+    };
+  };
+
+  var reloadMessages = function(){
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'GET',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages){
+      if (messages.length !== 0)
+      var insertHTML ='';
+      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      //メッセージが入ったHTMLに、入れ物ごと追加
+      $('.chat-main__message').append(insertHTML);
+      $('.chat-main__message').animate({ scrollTop: $('.chat-main__message')[0].scrollHeight})
+    })
+    .fail(function(){
+      alert('error');
+    });
+  };
+
   $('#new_message').on('submit',function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -53,4 +83,7 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     })
   });
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
